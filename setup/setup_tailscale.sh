f3a71fd48d5c9b82fc44f6f20d89f036772fad0f
phoeinx <hradilak@uni-potsdam.de>
2024-06-02 16:43:33 +0200

    Add setup folder.

---
+++ setup/setup_tailscale.sh
@@ -0,0 +1,16 @@
+#following tailscale installation instructions for Raspberry Pi here: https://tailscale.com/kb/1197/install-rpi-bullseye
+
+# 1. Install the apt-transport-https plugin
+sudo apt-get install apt-transport-https
+
+# 2. Add the Tailscale package signing key and repository
+curl -fsSL https://pkgs.tailscale.com/stable/raspbian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg > /dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/raspbian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+# 3. Install Tailscale
+sudo apt-get update
+sudo apt-get install tailscale
+
+# 4. Connect your machine to your Tailscale network and authenticate in your browser
+sudo tailscale up
+
